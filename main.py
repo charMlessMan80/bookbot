e1d6378cc1530d75caa05618ba3350b404ca7e92
charMlessMan80 <carpediem050180@gmail.com>
2025-01-26 15:25:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,54 @@
-def open_file(book_path):
-    with open(book_path, 'r') as file:
-        text = file.read()
-        return text
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
 
-def count_words(text):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+
+def get_num_words(text):
     words = text.split()
     return len(words)
 
-def char_count(text):
-    char_num = {}
-    for char in text.lower():
-        if char in char_num:
-            char_num[char] += 1
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
         else:
-            char_num[char] = 1
-    return char_num
+            chars[lowered] = 1
+    return chars
 
-def dict_to_list(char_num):
-    char_list = []
-    for key, value in char_num.items():
-        char_list.append((key, value))
-    return char_list
-def sort_on(dict_list):
-    return sorted(dict_list, key=lambda x: x[1], reverse=True)
 
-def print_report(sorted_list):
-    for char, count in sorted_list:
-        print(f'The \'{char}\' character was found {count} times')
 
-def main():
-    book_path = 'books/frankenstein.txt'
-    print(f'--- Begin report of {book_path} ---')
-    book = open_file(book_path)
-    word_count = (count_words(book))
-    print(f'{word_count} words found in the document\n')
-    char_num = char_count(book)
-    dict_list = dict_to_list(char_num)
-    sorted_list = sort_on(dict_list)
-    print_report(sorted_list)
-    print(f'--- End report ---')
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
 
 main()
